21b53b14db93b2ad5b75017d3230c4c13470ce54
Johan Martinez <johanmartinez@aptkeysolutions.com>
2022-01-05 14:36:41 -0500

    Version 1.001

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using File_I_O;
+
+namespace BaseballTweaker_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+          //  List<string> names = new List<string>();
+            var batting_stats = Input.RetrieveAllBattingRecords();
+            var pitching_stats = Input.RetrieveAllPitchingRecords();
+
+            OutputToConsole.OutputToConsoleAndFileBattingRecords(batting_stats);
+            OutputToConsole.OutputToConsoleAndFilePitchingRecords(pitching_stats);
+            
+
+         
+
+           //onsole.ReadLine();
+        }
+
+
+
+        
+    }
+}
